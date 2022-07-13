64343889082a14ba0a41c38ee3d4de6985c46e52
zhangshuai268 <81679435+zhangshuai268@users.noreply.github.com>
2022-07-13 15:20:11 +0800

    Update login_auth_test.go

--- service/wechat/login/login_auth_test.go
+++ service/wechat/login/login_auth_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestLogin_Auth(t *testing.T) {
-	openId := "oU9FgwpLnEtLduOShKwEZ8Rb02B0"
-	accessToken := "58_aef-0wLmtcU5Q6VZRQxoibBFlKUnJl5eg6nuGx7JXf6sxq26IdMjjJbTTkDiMwKiGVYsA75kbbH2zyQIG17Vj-_Ze_GH9uJ8wmAmapaD2jc"
+	openId := ""
+	accessToken := ""
 	auth, err := client.Auth(accessToken, openId)
 	if err != nil {
 		logger.Logger.Error(err)
